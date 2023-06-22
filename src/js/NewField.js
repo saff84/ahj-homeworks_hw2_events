cdf0259fbcd8eb2d44a1361a9827068c722a4e72
saff84 <saff84@mail.ru>
2023-06-22 20:57:04 +0300

    v4

--- src/js/NewField.js
+++ src/js/NewField.js
@@ -7,65 +7,79 @@ img.parentElement.removeChild(img);
 export default class NewField {
   constructor(board) {
     this.board = board;
+    this.timeout = null;
   }
 
   start() {
-
+    console.log('start')
     for (let i = 0; i < this.board ** 2; i += 1) {
       const div = document.createElement('div');
       div.classList.add('cell')
       plaingField.appendChild(div);
     }
-
-    this.randomGoblin();
+    this.timeout = setTimeout(this.randomGoblin, 1000)
+    this.listner();
   }
 
 
   genPosition() {
-
-      const parentChildrens = plaingField.children
-      return Math.floor(Math.random() * (parentChildrens.length - 1));
+      return Math.floor(1 + Math.random() * 15);
      }
 
+  randomGoblin(){
+    console.log('random')
 
-  randomGoblin() {
+    if (document.images[0]) {
+      console.log('random if')
 
-    let targetClick = 0;
-    let missedClick = 0;
-    const interval = setInterval(() => {
+      const activeDiv = document.images[0].parentElement;
+      activeDiv.removeChild(img);
+      activeDiv.classList.remove('activ')
+      }
+      console.log('random no if')
+    const childDivs = plaingField.children;
+    console.log(this)
+    const index = this.genPosition;
+    alert(index)
+    childDivs[index].classList.add('activ')
+    childDivs[index].appendChild(img);
 
-      if (document.images[0]) {
-            const activeDiv = document.images[0].parentElement;
-            activeDiv.removeChild(img);
-            activeDiv.classList.remove('activ')
-          }
+    this.timeout = setTimeout(this.randomGoblin.bind(this), 1000);        
 
-          const childDivs = plaingField.children;
-          let index = this.genPosition();
-          childDivs[index].classList.add('activ')
-          childDivs[index].appendChild(img);
+  }
 
-    }, 1000);
 
+  listner() {
+      let targetClick = 0;
+      let missedClick = 0;
+    console.log("listner")
     document.addEventListener("click", (event) => {
+      
       const eventTarget = event.target;
+      
       if (eventTarget.closest("img")) {
         targetClick++;
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(this.randomGoblin, 1000)
+
         points.innerText = `Набрано баллов ${targetClick}`;
         if (targetClick === 5){
           alert(`Игра окончена, Вы выйграли! `);
-          clearInterval(interval);
+          clearTimeout(this.timeout)
         } 
         
       } else {
         missedClick++;
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(this.randomGoblin, 1000)
         godlins.textContent = `Пропущено гоблинов ${missedClick}`
         if (missedClick === 5) {
           alert(`Игра окончена, пропущено ${missedClick } гоблинов `);
-          clearInterval(interval);
+          clearTimeout(this.timeout)
         }
       }
-
     });
   }
 }
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import NewField from "./NewField";
 
 const play = new NewField(4);
+// let genPosition = play.genPosition.bind(play)
 
-play.start();
+play.start()
  
